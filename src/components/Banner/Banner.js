e5608c4c88927b1959a15445704eda0ce0236ad3
Shahadat91 <shahadat20000@gmail.com>
2022-05-07 13:44:54 +0600

    banner created

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import { Carousel } from 'react-bootstrap';
+import banner1 from '../../Images/Banner/banner1.jpg'
+import banner2 from '../../Images/Banner/banner2.jpg'
+import banner3 from '../../Images/Banner/banner3.jpg'
 
 const Banner = () => {
     return (
-        <div>
-            <h2>This is Banner</h2>
-        </div>
+        <Carousel>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={banner1}
+      alt="First slide"
+    />
+  </Carousel.Item>
+
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={banner2}
+      alt="Second slide"
+    />
+  </Carousel.Item>
+
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={banner3}
+      alt="Third slide"
+    />
+  </Carousel.Item>
+</Carousel>
     );
 };
 
