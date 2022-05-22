c25aa38a515a574c4954e05922fe6c420c27a8a6
Shahadat91 <shahadat20000@gmail.com>
2022-05-22 14:38:49 +0600

    blogs created

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -6,22 +6,16 @@ const Blogs = () => {
       <div>
         <h4>Q-1: Difference between javascript and node js.</h4>
         <p>
-          Javascript is a programming language that is used for writing scripts
-          on the website. Javascript can only be run in the browsers.It is
-          basically used on the client-side.Javascript is capable enough to add
-          HTML and play with the DOM.Javascript is used in frontend development.
+          জাভাস্ক্রিপ্ট হচ্ছে একটি programming language যা ওয়েবসাইটে স্ক্রিপ্ট লেখার জন্য ব্যবহার হয়। জাভাস্ক্রিপ্ট শিধুমাত্র ব্রাউজারে চলতে পারে। সাধারণত জাভাস্ক্রিপ্ট ক্লাইন্ট সাইটে বেশি ব্যবহার করা হয়। জাভাস্ক্রিপ্ট এর মাধ্যমে HTML কোড লিখা সম্ভব। জাভাস্ক্রিপ্ট ফ্রন্টএন্ড ডেভেলপমেন্টে ব্যবহার করা হয়। 
         </p>
-        <p>
-          NodeJS is a Javascript runtime environment.We can run Javascript
-          outside the browser with the help of NodeJS.It is mostly used on the
-          server-side.Nodejs does not have capability to add HTML tags.Nodejs is
-          used in server-side development.
-        </p>
+
+        <p>NodeJS হচ্ছে জাভাস্ক্রিপ্ট এর runtime environment। NodeJS এর সাহায্যে ব্রাউজারের বাইরে আমরা জাভাস্ক্রিপ্ট চালাতে পারি। এটি বেশিরভাগ সার্ভার-সাইডে ব্যবহৃত হয়। HTML tag NodeJS এ লিখা সম্ভব হয় না। সার্ভার-সাইড ডেভেলপমেন্টে Nodejs ব্যবহার করা হয়।</p>
       </div>
+
       <div>
         <h4>Q-2: Differences between sql and nosql databases.</h4>
         <p>
-          SQL databases are primarily called as Relational Databases (RDBMS);
+          SQL databases are primarily called as Relational Databases (RDBMS),
           whereas NoSQL database are primarily called as non-relational or
           distributed database. SQL databases defines and manipulates data based
           structured query language (SQL). Seeing from a side this language is
@@ -67,15 +61,16 @@ const Blogs = () => {
         </p>
       </div>
       <div>
-        <h4>Q-3: When should you use `nodejs` and when should you use `mongodb` </h4>
-        <p>
-          When to use MongoDB? MongoDB can be used in places that require simple
-          queries, easy and fast integration of data and have data whose
-          structure changes constantly with time. Examples: 
-          E-commerce websites
-          Mobile applications
-           Blogs and content management portals Storing
-          geospatial data
+        <h4>Q-3: When should you use nodejs and when should you use mongodb? </h4>
+        <p>Node js হল একটি ওপেন সোর্স, সার্ভার-সাইড এবং নেটওয়ার্কিং অ্যাপ্লিকেশন তৈরির জন্য ক্রস-প্ল্যাটফর্ম runtime environment. Node.js অ্যাপ্লিকেশনগুলি জাভাস্ক্রিপ্টে লেখা হয়। Node js বিভিন্ন জাভাস্ক্রিপ্ট মডিউলের একটি সমৃদ্ধ লাইব্রেরি প্রদান করে যা Node js ব্যবহার করে ওয়েব অ্যাপ্লিকেশনের development  অনেক সহজ করে।</p>
+
+        <p>মঙ্গোডিবি এমন জায়গায় ব্যবহার করা যেতে পারে যেখানে simple
+          queries, easy and fast integration of data আছে এবং যার গঠন সময়ের সাথে সাথে ক্রমাগত পরিবর্তিত হয়।
+          উদাহরণ: 
+          ১. E-commerce websites
+          ২. Mobile applications
+          ৩. Blogs and content management portals Storing। 
+          
         </p>
       </div>
     </div>
