023e1ae342da9df7a9fa64f83c72d00cf5dccb6f
Shahadat91 <shahadat20000@gmail.com>
2022-05-23 17:04:18 +0600

    404 update

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NotFound = () => {
     return (
-        <div>
+        <div className='text-center'>
             <h2>Page You are looking for not found</h2>
             <p>404</p>
         </div>
